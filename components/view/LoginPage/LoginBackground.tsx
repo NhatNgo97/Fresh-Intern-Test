b15394566565dee6e0298345355b8b1f39869296
NhatNgo97 <nhatngo9799@gmail.com>
2022-10-14 17:17:53 +1100

    background login

---
+++ components/view/LoginPage/LoginBackground.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+import LoginBackgroundImg from '../../../assets/Illustration.png';
+
+
+
+const LoginBackground : React.FC<{}> = props => {
+  return (<div className="lg:w-[30%] md:w-[35%] hidden md:flex h-full relative">
+  <Image
+    objectFit='cover'
+    layout='fill'
+    src={LoginBackgroundImg}
+    alt="" />
+</div>);
+}
+
+export default LoginBackground;
