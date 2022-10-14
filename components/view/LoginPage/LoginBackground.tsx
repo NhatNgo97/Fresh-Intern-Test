f46c5a7031c5d8f1abac8776ef60dce9114b8673
NhatNgo97 <nhatngo9799@gmail.com>
2022-10-14 18:50:03 +1100

    task 3

--- components/view/LoginPage/LoginBackground.tsx
+++ components/view/LoginPage/LoginBackground.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 import LoginBackgroundImg from '../../../assets/Illustration.png';
 
-
-
-const LoginBackground : React.FC<{}> = props => {
-  return (<div className="lg:w-[30%] md:w-[35%] hidden md:flex h-full relative">
-  <Image
-    objectFit='cover'
-    layout='fill'
-    src={LoginBackgroundImg}
-    alt="" />
-</div>);
-}
+const LoginBackground: React.FC<{}> = (props) => {
+  return (
+    <div className="lg:w-[30%] md:w-[35%] hidden md:flex h-full relative">
+      <Image objectFit="cover" layout="fill" src={LoginBackgroundImg} alt="" />
+    </div>
+  );
+};
 
 export default LoginBackground;
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,72 +1,28 @@
 import { setCookies } from 'cookies-next';
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  success: boolean,
-  message: string,
-  accessToken?: string,
-}
-
 // TODO: Task 3
 // Build an API to handle login requests.
-const handler: NextApiHandler = async ( req : NextApiRequest,res :NextApiResponse<Data>) => {
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
   //This is my JWT Login way
-  try{
+  try {
     if (req.method === 'POST') {
       //check email in database. in this case ,i use Mongoose to check it
-      const user = await User.findOne({ email: req.body.email });
-      if (!user) {
-        res.status(401).json({
-          success: false,
-          message: "Email or Password is incorrect, Please try again",
-          accessToken: undefined,
-        });
-        return;
-      }
+      const email = req.body.email;
+      const password = req.body.password;
 
-      // encode the password and compare in the db.
-      const validPassword = await bcrypt.compare(
-        req.body.password,
-        user.password
-      );
-      if (!validPassword) {
-        res.status(401).json({
-          success: false,
-          message: "Email or Password is incorrect, Please try again",
-          accessToken: undefined
-        });
-        return;
-      }
-
-      //if email and password are correct so we start
-      //generate access Token and refresh Token
-      //access is gonna be saved in redux store or useContext 
-      //for the verifying while refresh token is saved in cookie.
-      const accessToken = "a generated access token"
-      const refreshToken = "a generated refresh token"
-
-      //STORE REFRESH TOKEN IN COOKIE
-      setCookies('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: true,
-        path: '/',
-        sameSite: 'none',
-      });
-
-      res.status(200).json({
-        success: true,
-        message: "succeed",
-        accessToken: accessToken,
+      return res.status(200).json({
+        email,
+        password,
       });
     } else {
       // Handle any other HTTP method
     }
-  } catch(err) {
-    res.status(500).json({
-      success: false,
-      message: "ERROR",
-      accessToken: undefined,
-    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
